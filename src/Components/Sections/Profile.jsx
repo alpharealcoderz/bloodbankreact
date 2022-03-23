f16273652091cef99dff71b7e2f0359cf21388f7
Shivam <codewrapperz360@gmail.com>
2022-03-23 16:36:31 +0530

    tempnew

--- src/Components/Sections/Profile.jsx
+++ src/Components/Sections/Profile.jsx
@@ -8,6 +8,7 @@ import Footer from "../Admin/layout/Footer";
 import { api_base_url } from "../../Constants";
 export default function Profile() {
   const dispatch = useDispatch();
+  const [requester,setRequester]=useState([]);
   const [columns, setColumns] = useState([
     {
       title: "Name",
@@ -54,7 +55,52 @@ export default function Profile() {
       dataIndex: "hospital_state",
       key: "hospital_state",
     },
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "",
+    },
   ]);
+
+  const [columnss, setColumnss] = useState([
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "adress",
+    },
+    {
+      title: "City",
+      dataIndex: "city",
+      key: "city",
+    },
+    {
+      title: "State",
+      dataIndex: "state",
+      key: "state",
+    },
+ 
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "",
+    },
+  ]);
+
 
   const [data, setData] = useState([]);
   const [details, setDetails] = useState(
@@ -75,6 +121,23 @@ export default function Profile() {
       // data && data.status == "success" && setData(data.requestData);
     }
   }, []);
+
+  useEffect(() => {
+    if (localStorage.getItem("token") == null) window.location.hash = "home";
+    else {
+      axios
+        .get(`${api_base_url}/raisedlist`, {
+          id: details.id,
+        })
+        .then((res) => {
+          // console.log("shiv", res.data[0].requester_id);
+          setRequester(res.data);
+        });
+
+      // data && data.status == "success" && setData(data.requestData);
+    }
+  }, []);
+console.log('abcd',requester)
   const handleDetails = (e) => {
     let object = {};
     object[e.target.name] = e.target.value;
@@ -82,7 +145,7 @@ export default function Profile() {
   };
   const update = (e) => {
     e.preventDefault();
-    console.log("saransh", details);
+    // console.log("saransh", details);
     updateUser(details);
   };
   if (localStorage.getItem("token") == null) return <></>;
@@ -142,7 +205,182 @@ export default function Profile() {
           {/* <div class="col-6"> */}
           <div class="heading-area">
             <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <h2 class="title">Your Request</h2>
+              <h2 class="title">Your Request As Needy</h2>
+              <div></div>
+            </div>
+            <h6 class="sub-title main-color">Omniscient BloodBank</h6>
+          </div>
+          <Modal
+            title="Edit Profile"
+            visible={visible}
+            onOk={(e) => update(e)}
+            onCancel={() => setVisible(false)}
+          >
+            <form
+              class="contact-form"
+              id="contact-form-data"
+              onSubmit={(e) => {
+                update(e);
+              }}
+            >
+              <div class="row">
+                <div class="col-sm-12" id="result"></div>
+                <div class="col-lg-6">
+                  <div class="form-group">
+                    <input
+                      class="form-control"
+                      type="text"
+                      placeholder="Full Name"
+                      name="name"
+                      value={details?.name}
+                      onChange={(e) => {
+                        handleDetails(e);
+                      }}
+                    />
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <div class="form-group">
+                    <input
+                      class="form-control"
+                      type="email"
+                      placeholder="Email"
+                      name="email"
+                      value={details?.email}
+                      onChange={(e) => {
+                        handleDetails(e);
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-6">
+                  <div class="form-group">
+                    <input
+                      class="form-control"
+                      type="number"
+                      placeholder="Phone"
+                      name="phone"
+                      value={details?.phone}
+                      onChange={(e) => {
+                        handleDetails(e);
+                      }}
+                    />
+                  </div>
+                </div>
+
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <input
+                      class="form-control"
+                      type="number"
+                      placeholder="Age"
+                      value={details?.age}
+                      name="age"
+                      onChange={(e) => {
+                        handleDetails(e);
+                      }}
+                    />
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <select
+                      class="form-control"
+                      placeholder="Gender"
+                      defaultValue={details?.gender}                    
+                      name="gender"
+                      onChange={(e) => {
+                        handleDetails(e);
+                      }}
+                    >
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="row">
+                  <div class="col-lg-9">
+                    <input
+                      class="form-control"
+                      name="address"
+                      onChange={(e) => {
+                        handleDetails(e);
+                      }}
+                      value={details?.address}
+                      placeholder="address"
+                    ></input>
+                  </div>
+                  <div class="col-sm-3">
+                    <input
+                      class="form-control"
+                      name="blood_type"
+                      value={details?.blood_type}
+                      onChange={(e) => {
+                        handleDetails(e);
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input
+                      class="form-control"
+                      type="text"
+                      placeholder="City"
+                      name="city"
+                      value={details?.city}
+                      onChange={(e) => {
+                        handleDetails(e);
+                      }}
+                    />
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input
+                      class="form-control"
+                      type=""
+                      placeholder="State"
+                      value={details?.state}
+                      name="state"
+                      onChange={(e) => {
+                        handleDetails(e);
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              {/* <div class="form-group">
+                <input
+                  class="form-control"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  onChange={(e) => {
+                    handleDetails(e);
+                  }}
+                />
+              </div> */}
+            </form>
+          </Modal>
+          <Table dataSource={data} columns={columns} scroll={{ x: 400 }} />;
+          <Table dataSource={requester} columns={columnss} scroll={{ x: 400 }} />;
+        </div>
+      </div>
+      <div class="container">
+
+        <div class=" align-items-lg-center dot-box">
+          {/* <div class="col-6"> */}
+          <div class="heading-area">
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <h2 class="title">Donor Request</h2>
               <div></div>
             </div>
             <h6 class="sub-title main-color">Omniscient BloodBank</h6>
